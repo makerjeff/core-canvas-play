eb0c28caf911ae0887be3668f6774bbefc318c97
Jeff Wu <makerjeffwu@gmail.com>
2017-01-25 11:39:37 -0800

    working modalMaker, pushing to pull from work machine

--- public/js/timeoutModalModule.js
+++ public/js/timeoutModalModule.js
@@ -12,7 +12,29 @@
 var Modal = {
 
     create: function(timeout){
+        var container = document.createElement('div');
+        container.id = 'timeoutModalContainer';
+        var veil = document.createElement('div');
+        var box = document.createElement('div');
+        var spinner = document.createElement('div');
 
+        container.classList.add('timeoutModalContainer');
+        veil.classList.add('timeoutModalVeil');
+        box.classList.add('timeoutModalBox');
+        spinner.classList.add('timeoutModalSpinner');
+
+        box.appendChild(spinner);
+        container.appendChild(veil);
+        container.appendChild(box);
+        document.body.appendChild(container);
+
+        spinner.addEventListener('click', function(e){
+            Modal.destroy();
+        });
+    },
+    destroy: function() {
+        var modalContainer = document.getElementById('timeoutModalContainer');
+        modalContainer.parentNode.removeChild(modalContainer);
     }
 };
 // TIMEOUT MODAL MODULE ===================
